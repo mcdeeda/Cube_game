a7025d0804da64dbc4f596c6657bd065f0c2071c
mcdeeda <emcde94@gmail.com>
2017-12-18 14:55:34 -0500

    adding particle effects on wallgrind
    all new stuff contained within folder edan-stuff in scene level05

---
+++ Assets/edan-stuff/particledrag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particledrag : MonoBehaviour {
+    public PlayerMovement movement;
+    public ParticleSystem particles;
+	// Use this for initialization
+	void Start () {
+        particles.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (movement.isOnWall)
+        {
+            if (movement.wallSide == "Left")
+            {
+                var sh = particles.shape;
+                sh.position = new Vector3(-0.5f, 0, -0.2f);
+                particles.Play();
+            }
+            if (movement.wallSide == "Right")
+            {
+                var sh = particles.shape;
+                sh.position = new Vector3(0.5f, 0, -0.2f);
+                particles.Play();
+            }
+        }
+        else
+        {
+            particles.Stop();
+        }
+    }
+}
